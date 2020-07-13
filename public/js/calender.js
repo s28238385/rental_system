df35c6d9817d20a6eb63bbecb615fedddc7d8d3e
Lexlie <l.exlie823@gmail.com>
2020-07-13 16:48:35 +0800

    add to certain week function

--- public/js/calender.js
+++ public/js/calender.js
@@ -13,8 +13,9 @@ Date.prototype.minusDay = function(days){
 }
 
 Date.prototype.getWeek = function(){
-    let day = this.getDay();
     let sundayOfShowingWeek = new Date();
+    
+    let day = this.getDay();
 
     switch(day){
         case 0:
@@ -45,18 +46,18 @@ Date.prototype.getWeek = function(){
             sundayOfShowingWeek = this.minusDay(6);
             break;
     }
-
+    
     return sundayOfShowingWeek;
 }
 
 function fillInDates(){
-    document.getElementById('Sun').innerHTML = ( sundayOfShowingWeek.getMonth() + 1 ) + "/" + sundayOfShowingWeek.getDate() + "<br>（日）";
-    document.getElementById('Mon').innerHTML = ( sundayOfShowingWeek.addDay(1).getMonth() + 1 ) + "/" + sundayOfShowingWeek.addDay(1).getDate() + "<br>（一）";
-    document.getElementById('Tue').innerHTML = ( sundayOfShowingWeek.addDay(2).getMonth() + 1 ) + "/" + sundayOfShowingWeek.addDay(2).getDate() + "<br>（二）";
-    document.getElementById('Wed').innerHTML = ( sundayOfShowingWeek.addDay(3).getMonth() + 1 ) + "/" + sundayOfShowingWeek.addDay(3).getDate() + "<br>（三）";
-    document.getElementById('Thu').innerHTML = ( sundayOfShowingWeek.addDay(4).getMonth() + 1 ) + "/" + sundayOfShowingWeek.addDay(4).getDate() + "<br>（四）";
-    document.getElementById('Fri').innerHTML = ( sundayOfShowingWeek.addDay(5).getMonth() + 1 ) + "/" + sundayOfShowingWeek.addDay(5).getDate() + "<br>（五）";
-    document.getElementById('Sat').innerHTML = ( sundayOfShowingWeek.addDay(6).getMonth() + 1 ) + "/" + sundayOfShowingWeek.addDay(6).getDate() + "<br>（六）";
+    document.getElementById('Sun').innerHTML = sundayOfShowingWeek.getFullYear() + "<br>" + ( sundayOfShowingWeek.getMonth() + 1 ) + "/" + sundayOfShowingWeek.getDate() + "<br>（日）";
+    document.getElementById('Mon').innerHTML = sundayOfShowingWeek.addDay(1).getFullYear() + "<br>" + ( sundayOfShowingWeek.addDay(1).getMonth() + 1 ) + "/" + sundayOfShowingWeek.addDay(1).getDate() + "<br>（一）";
+    document.getElementById('Tue').innerHTML = sundayOfShowingWeek.addDay(2).getFullYear() + "<br>" + ( sundayOfShowingWeek.addDay(2).getMonth() + 1 ) + "/" + sundayOfShowingWeek.addDay(2).getDate() + "<br>（二）";
+    document.getElementById('Wed').innerHTML = sundayOfShowingWeek.addDay(3).getFullYear() + "<br>" + ( sundayOfShowingWeek.addDay(3).getMonth() + 1 ) + "/" + sundayOfShowingWeek.addDay(3).getDate() + "<br>（三）";
+    document.getElementById('Thu').innerHTML = sundayOfShowingWeek.addDay(4).getFullYear() + "<br>" + ( sundayOfShowingWeek.addDay(4).getMonth() + 1 ) + "/" + sundayOfShowingWeek.addDay(4).getDate() + "<br>（四）";
+    document.getElementById('Fri').innerHTML = sundayOfShowingWeek.addDay(5).getFullYear() + "<br>" + ( sundayOfShowingWeek.addDay(5).getMonth() + 1 ) + "/" + sundayOfShowingWeek.addDay(5).getDate() + "<br>（五）";
+    document.getElementById('Sat').innerHTML = sundayOfShowingWeek.addDay(6).getFullYear() + "<br>" + ( sundayOfShowingWeek.addDay(6).getMonth() + 1 ) + "/" + sundayOfShowingWeek.addDay(6).getDate() + "<br>（六）";
 }
 
 function toPreviousWeek(){
@@ -73,6 +74,14 @@ function toThisWeek(){
 
 function toNextWeek(){
     sundayOfShowingWeek = sundayOfShowingWeek.addDay(7);
+    
+    fillInDates();
+}
+
+function toCertainWeek(){
+    let insert = new Date(document.getElementById('search-block').value);
+
+    sundayOfShowingWeek = insert.getWeek();
     
     fillInDates();
 }
