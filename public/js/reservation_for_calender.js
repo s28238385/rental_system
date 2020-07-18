9ba7bb54d59d205638194aab797db37d8b7529d7
HsinlunChiu <gfpss95114@gmail.com>
2020-07-18 14:56:58 +0800

    0718 pull for new calender

--- public/js/reservation_for_calender.js
+++ public/js/reservation_for_calender.js
@@ -3,6 +3,10 @@ $('document').ready(function(){
 
     //$('.calender-data').children('#Sun').text(date+" is here");
         //console.log(date+' is found');//test
+    let test = $('#Sun-12').parent().siblings('.calender-data').children('#Sun').html()
+    console.log(test);
+    
+    $('#Sun-12').append(date);
 
     
 });
