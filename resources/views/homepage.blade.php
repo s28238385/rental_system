4ee82fce56f41ec1e1e39452c2e5cb59613b8462
FOBshippingpoint <aulaulql4ql4xj6@gmail.com>
2020-07-23 16:47:38 +0800

    add href for newapply and add alert to homepage.

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,6 +5,8 @@
 @endsection
 
 @section('content')
+
+
 <div class="homepage vh-100">
     <div class="container vh-100 d-flex justify-content-center">
         <div class="d-flex align-items-center">
@@ -14,51 +16,80 @@
                 </div>
                 <div class="pt-3 d-flex justify-content-center">
                     <div>
-                        <a href="" class="text-decoration-none"><h3 class="px-3 py-1 yellow-index" href="#">教室預約狀況</h3></a>
+                        <a href="" class="text-decoration-none">
+                            <h3 class="px-3 py-1 yellow-index" href="#">教室預約狀況</h3>
+                        </a>
                         <div>
                             <h3 type="button" id="borrow" class="px-3 yellow-index" href="#">教室設備借用</h3>
                             <div id="subindex" class="d-none pb-3">
-                                <a class="text-decoration-none" href="#"><h5 class="px-2 yellow-subindex">新增申請</h5></a>
-                                <a class="text-decoration-none" href="#"><h5 class="px-2 yellow-subindex">申請清單</h5></a>
+                                <a class="text-decoration-none" href="{{route('newapply.create')}}">
+                                    <h5 class="px-2 yellow-subindex">新增申請</h5>
+                                </a>
+                                <a class="text-decoration-none" href="#">
+                                    <h5 class="px-2 yellow-subindex">申請清單</h5>
+                                </a>
                             </div>
                         </div>
                         @if(Auth::check())
-                            <div>
-                                <h3 type="button" id="appointment" class="px-3 yellow-index" href="#">教室預約</h3>
-                                <div id="subindex" class="d-none pb-3">
-                                    <a class="text-decoration-none" href=""><h5 class="px-2 yellow-subindex">單次預約</h5></a>
-                                    <a class="text-decoration-none" href=""><h5 class="px-2 yellow-subindex">長期預約</h5></a>
-                                </div>
+                        <div>
+                            <h3 type="button" id="appointment" class="px-3 yellow-index" href="#">教室預約</h3>
+                            <div id="subindex" class="d-none pb-3">
+                                <a class="text-decoration-none" href="">
+                                    <h5 class="px-2 yellow-subindex">單次預約</h5>
+                                </a>
+                                <a class="text-decoration-none" href="">
+                                    <h5 class="px-2 yellow-subindex">長期預約</h5>
+                                </a>
                             </div>
-                            @can('manager')
-                                <div>
-                                    <h3 type="button" id="equipment" class="px-3 yellow-index" href="#">設備管理</h3>
-                                    <div id="subindex" class="d-none pb-3">
-                                        <a class="text-decoration-none" href="{{route('equipment.add')}}"><h5 class="px-2 yellow-subindex">新增設備</h5></a>
-                                        <a class="text-decoration-none" href="#"><h5 class="px-2 yellow-subindex">設備清單</h5></a>
-                                    </div>
-                                </div>
-                            @endcan
+                        </div>
+                        @can('manager')
+                        <div>
+                            <h3 type="button" id="equipment" class="px-3 yellow-index" href="#">設備管理</h3>
+                            <div id="subindex" class="d-none pb-3">
+                                <a class="text-decoration-none" href="{{route('equipment.add')}}">
+                                    <h5 class="px-2 yellow-subindex">新增設備</h5>
+                                </a>
+                                <a class="text-decoration-none" href="#">
+                                    <h5 class="px-2 yellow-subindex">設備清單</h5>
+                                </a>
+                            </div>
+                        </div>
+                        @endcan
                         @endif
                     </div>
                 </div>
                 @if(Auth::check())
-                    <div class=" mt-4 d-flex inline-flex justify-content-center">
-                        @can('manager')
-                            <a class="text-decoration-none px-1" href="{{route('user.userlist')}}"><h6 class="text-muted">管理使用者</h6></a>
-                        @endcan
-                        <a class="text-decoration-none px-1" href="{{route('user.changepw')}}"><h6 class="text-muted">修改密碼</h6></a>
-                        <a class="text-decoration-none px-1" href="{{route('user.logout')}}"><h6 class="text-muted">登出</h6></a>
-                    </div>
+                <div class=" mt-4 d-flex inline-flex justify-content-center">
+                    @can('manager')
+                    <a class="text-decoration-none px-1" href="{{route('user.userlist')}}">
+                        <h6 class="text-muted">管理使用者</h6>
+                    </a>
+                    @endcan
+                    <a class="text-decoration-none px-1" href="{{route('user.changepw')}}">
+                        <h6 class="text-muted">修改密碼</h6>
+                    </a>
+                    <a class="text-decoration-none px-1" href="{{route('user.logout')}}">
+                        <h6 class="text-muted">登出</h6>
+                    </a>
+                </div>
                 @else
-                    <div class="mt-4">
-                        <a class="text-decoration-none" href="#"><h6 class="text-muted">管理者登入</h6></a>
-                    </div>
+                <div class="mt-4">
+                    <a class="text-decoration-none" href="#">
+                        <h6 class="text-muted">管理者登入</h6>
+                    </a>
+                </div>
                 @endif
             </div>
         </div>
     </div>
 </div>
+
+<?php 
+if(isset($alert)){
+    echo $alert;
+}
+?>
+
 @endsection
 
 @section('script')
