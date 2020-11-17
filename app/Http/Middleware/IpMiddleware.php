a9595c454f8ce475b83a634abe82f10bcd2e01c2
Lexlie <l.exlie823@gmail.com>
2020-11-17 19:38:13 +0800

    corrected blocked ip

--- app/Http/Middleware/IpMiddleware.php
+++ app/Http/Middleware/IpMiddleware.php
@@ -15,7 +15,7 @@ class IpMiddleware
      */
     public function handle($request, Closure $next)
     {
-        $pattern = "/^127.0.\d{1~3}.\d{1~3}$/";
+        $pattern = "/^140.115.\d{1~3}.\d{1~3}$/";
 
         if(preg_match($pattern, $request->ip()) == 0 && preg_match("/not_allowed_ip$/", $request->url()) == 0){
             return redirect('not_allowed_ip');
